689ffe3e8543b73e9b4cc89d184f6b13fbf74d12
Ian Macalinao <ian@giza.us>
2014-01-02 07:38:40 -0600

    Added dictionaries for account creation.
    Signed-off-by: Ian Macalinao <ian@giza.us>

---
+++ create-accounts.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Creates accounts from CAPTCHA solutions, using a
+wordbank to generate realistic-sounding usernames.
+"""
+
+def main():
+    """
+    Main function of create-accounts.
+    """
+
+if __name__ == "__main__":
+    main()
