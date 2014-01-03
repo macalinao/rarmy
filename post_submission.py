56f5fbd6ce4edf8fa6c5ee4cb54e7868f4cfa5d5
Ian Macalinao <ian@giza.us>
2014-01-03 11:58:15 -0600

    Add Danbooru grabber
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- post_submission.py
+++ post_submission.py
@@ -1,5 +1,43 @@
 from lib import army
+import requests
+from BeautifulSoup import BeautifulSoup
+import json
 
-s = army.Soldier({ 'user': 'SelfishArithmetic', 'pass': '--censored from git--' })
-s.login('46.249.66.50:8080')
-s.vote('t3_1ubfuy')
+config = json.load(open('config/config.json'))
+
+def danbooru_grab(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    full_title = soup.title.string
+
+    s1 = full_title.find('(')
+    s2 = full_title.find(')')
+
+    title = full_title[0:s1].strip().title()
+    source = full_title[s1 + 1:s2].strip().title()
+    reddit_title = title + ' [' + source + ']'
+    reddit_comment = '[Source - Danbooru](' + url + ')'
+
+    img = 'http://danbooru.donmai.us/' + soup.find(alt='Sample')['src']
+
+    headers = {'Authorization': 'Client-ID ' + config['imgur']['client_id']}
+    payload = {
+        'image': img,
+        'title': title,
+        'description': full_title
+    }
+
+    r = requests.post('https://api.imgur.com/3/image', headers=headers, data=payload)
+    link = r.json()['data']['link']
+
+    return {
+        'reddit_title': reddit_title,
+        'reddit_comment': reddit_comment,
+        'link': link
+    }
+
+post = danbooru_grab('http://danbooru.donmai.us/posts/1583318')
+
+# s = army.Soldier({ 'user': 'SelfishArithmetic', 'pass': '--censored from git--' })
+# s.login('46.249.66.50:8080')
+# s.vote('t3_1ubfuy')
