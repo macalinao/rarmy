7c58e7a6f0f9a45e273508b35434e7956ef50f2e
Ian Macalinao <ian@giza.us>
2014-01-17 22:39:00 -0600

    Removed saving the army in upvote_submission
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- examples/upvote_submission.py
+++ examples/upvote_submission.py
@@ -4,7 +4,6 @@ url = raw_input('Enter a Reddit post to mass-upvote: ')
 amt = raw_input('Enter the amount of upvotes desired: ')
 
 a = Army(int(amt))
-a.save()
 a.vote(url)
 
 print 'Check the post; it should have +' + amt + ' upvotes.'
